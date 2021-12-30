a843db6cd7909b6c0045f3be60e1a4c1939b3057
Ricardo <ricardo-jr-1@live.com>
2021-12-30 12:27:03 -0300

    projeto dio

---
+++ src/test/java/com/digitalinnovation/herosapi/HerosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.herosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
